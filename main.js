cd63c37ec6521b46b832c06260137edd30b42822
Chris-dagostino <dagostino.christian94@gmail.com>
2022-01-20 15:08:39 +0100

    generatore di password

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 
 let name = prompt ('inserisci il tuo nome');
-document.getElementById ('firstName').innerHTML=name;
 
 let lastName = prompt ('inserisci il tuo cognome');
-document.getElementById ('lastName').innerHTML=lastName;
 
 let color = prompt ('inserisci il tuo colore preferito');
-document.getElementById ('color').innerHTML=color;
 
 let password = name + lastName + color + "21";
 document.getElementById ('password').innerHTML=password
